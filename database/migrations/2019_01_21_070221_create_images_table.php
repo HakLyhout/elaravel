4378020f8ea99e69106ee66f80529d2934825492
Asus <haklyhout1996@gmail.com>
2019-01-21 14:25:00 +0700

    1 more migration

---
+++ database/migrations/2019_01_21_070221_create_images_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_images', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('pro_id');
+            $table->string('img_name');
+            $table->string('img_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_images');
+    }
+}
